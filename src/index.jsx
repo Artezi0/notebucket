275587b95a90c69b6ba306f9d5c97adb565c57bd
Artezi0 <193.rayyan@mpuin-jkt.sch.id>
2022-06-30 15:07:09 +0700

    Fixed some issues

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
+
 )
