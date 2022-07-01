2d1659487c3a81802e6096f1cbeb3481cbb4dc65
Artezi0 <193.rayyan@mpuin-jkt.sch.id>
2022-07-01 16:33:51 +0700

    Updates manifest.json

--- vite.config.js
+++ vite.config.js
@@ -23,31 +23,25 @@ export default defineConfig({
         start_url: 'https://notebuckett.netlify.app/',
         icons: [
           {
-            src: "icon-192x192.png",
+            src: "pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
-            purpose: "maskable"
+            purpose: "any maskable"
           },
           {
-            src: "icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any"
-          },
-          {
-            src: "icon-256x256.png",
+            src: "pwa-256x256.png",
             sizes: "256x256",
             type: "image/png",
             purpose: "any"
           },
           {
-            src: "icon-384x384.png",
+            src: "pwa-384x384.png",
             sizes: "384x384",
             type: "image/png",
             purpose: "any"
           },
           {
-            src: "/icon-512x512.png",
+            src: "pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any"
