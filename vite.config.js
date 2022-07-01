db6397987205ca9c82ca96e089c17b5d9cc40632
Artezi0 <193.rayyan@mpuin-jkt.sch.id>
2022-07-01 15:41:02 +0700

    Updates manifest.json

--- vite.config.js
+++ vite.config.js
@@ -19,31 +19,31 @@ export default defineConfig({
         background_color: '#ffffff',
         display: 'fullscreen',
         scope: '/',
-        start_url: 'https://notebucket.netlify.app/',
+        start_url: 'https://notebuckett.netlify.app/',
         icons: [
           {
             "src": "src/assets/icon-192x192.png",
             "sizes": "192x192",
             "type": "image/png",
-            "purpose": "maskable"
+            "purpose": "any"
           },
           {
             "src": "src/assets/icon-256x256.png",
             "sizes": "256x256",
             "type": "image/png",
-            "purpose": "maskable"
+            "purpose": "any"
           },
           {
             "src": "src/assets/icon-384x384.png",
             "sizes": "384x384",
             "type": "image/png",
-            "purpose": "maskable"
+            "purpose": "any"
           },
           {
             "src": "src/assets/icon-512x512.png",
             "sizes": "512x512",
             "type": "image/png",
-            "purpose": "maskable"
+            "purpose": "any"
           }
         ]
       }
