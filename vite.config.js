950ddc83f7a9b0801066c54da50b821195e2ac9e
Artezi0 <193.rayyan@mpuin-jkt.sch.id>
2022-07-01 17:58:46 +0700

    Dropped PWA

--- vite.config.js
+++ vite.config.js
@@ -7,41 +7,5 @@ export default defineConfig({
   plugins: [
     react(), 
     viteCompression(),
-    VitePWA({ 
-      registerType: 'autoUpdate', 
-      injectRegister: 'auto',
-      workbox: {
-        globPatterns: ['**/*{js,css,html,ico,png,svg}']  
-      },
-      manifest: {
-        name: 'Notebcuket',
-        short_name: 'Notebucket',
-        description: 'Organize and style your notes freely',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'fullscreen',
-        orientation: 'portrait',
-        scope: '/',
-        start_url: 'https://notebuckett.netlify.app/',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      }
-    })
   ]
 })
